5a8f2789947623fcb1b159aedfba3cf76f3bc0d2
Alstromg <ja358577@gmail.com>
2023-09-28 16:55:43 -0500

    Entrega 2

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
 const express = require('express');
-const http = require('http'); // Importa el módulo http
+const http = require('http');
+const mongoose = require("mongoose")
 const socketIO = require('socket.io');
-const products = require('./js/products');
-const carts = require('./js/carts');
 const handlebars = require("express-handlebars")
 const viewsRouter = require('./routers/view.router')
+const viewsCart = require('./routers/viewsCart')
+const cartRouter = require("./routers/cartRouter")
+const productRouter = require("./routers/productRouter")
 const path = require('path');
 
-const app = express();
 
+const app = express();
+const PORT = 8080
 app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 app.use(express.static('./src/public'));
 app.engine('handlebars', handlebars.engine());
 app.set('views', './src/views')
@@ -18,31 +22,33 @@ app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => res.render('index'))
 app.use('/products', viewsRouter);
-app.get('/api/products', products.getAllProducts);
-app.get('/api/products/:pid', products.getProductById);
-app.post('/api/products', products.addProduct);
-app.put('/api/products/:pid', products.updateProduct);
-app.delete('/api/products/:pid', products.deleteProduct);
-
-
-app.post('/api/carts', carts.postCart);
-app.get('/api/carts/:cid', carts.getCart);
-app.post('/api/carts/:cid/product/:pid', carts.addProductToCart);
+app.use('/cart', viewsCart)
+app.use('/api/products', productRouter);
+app.use('/api/carts', cartRouter);
 
 
 const httpServer = http.createServer(app);
-const io = socketIO(httpServer); 
+const io = socketIO(httpServer);
 
-const PORT = process.env.PORT || 8080;
-httpServer.listen(PORT, () => {
-    console.log(`Servidor activo en el puerto ${PORT}`);
-});
-io.on("connection", socket => {
-    socket.on("productList", data => {
-            io.emit("updatedProducts", data);
-        
+(async () => {
+  try {
+    await mongoose.connect("mongodb+srv://Alstromg:320maVEZ@atlascluster.56bn6jf.mongodb.net/", {
+      dbName: "Productos",
+      useNewUrlParser: true, 
+      useUnifiedTopology: true,
     });
-});
+    httpServer.listen(8080, () => console.log("Servidor en línea"));
+    
+  } catch (err) {
+    console.error("Error al conectar a la base de datos:", err.message);
+  }
+})();
 
+io.on("connection", socket => {
+  socket.on("productList", data => {
+    io.emit("updatedProducts", data);
+  });
+});
+module.exports = PORT;
 
 
--- src/routers/view.router.js
+++ src/routers/view.router.js
@@ -1,18 +1,56 @@
 const  {Router}  = require('express')
-const ProductManger = require('../js/ProductManager')
-
+const baseModel = require("../models/basemModel")
+const cartModel = require('../models/cartModel')
 const router = Router()
-const productManager = new ProductManger('./src/json/productos.json')
-
+const {getProducts} = require('../public/constroladorProduct')
+const {PORT} = require('../app')
 
 router.get('/', async (req, res) => {
-    const products = await productManager.getProductos()
-    res.render('home' , {products})
+    try {
+    const carro = new cartModel({ products: []})
+    const saveCart = await carro.save()
+    const cart = saveCart._id
+    const result = await getProducts(req, res)
+    if (result.statusCode === 200) {
+        const totalPages = []
+        let link
+        for (let index = 1; index <= result.response.totalPages; index++) {
+            if (!req.query.page) {
+                link = `http://${req.hostname}:${PORT}${req.originalUrl}&page=${index}`
+            } else {
+                const modifiedUrl = req.originalUrl.replace(`page=${req.query.page}`, `page=${index}`)
+                link = `http://${req.hostname}:${PORT}${modifiedUrl}`
+            }
+            totalPages.push({ page: index, link })
+        }
+        res.render('home', {
+            cart,
+            products: result.response.payload, paginateInfo: {
+                hasPrevPage: result.response.hasPrevPage,
+                hasNextPage: result.response.hasNextPage,
+                prevLink: result.response.prevLink,
+                nextLink: result.response.nextLink,
+                totalPages
+            }
+        })
+    } else {
+        res.status(result.statusCode).json({ status: 'error', error: result.response.error })
+    }
+}catch(err){
+    console.error("Error al crear el carrito:",);
+    res.status(500).json({ status: "error", error: "Error interno del servidor" });
+}
 })
 
-router.get('/realtimeProducts', async (req, res) => {
-    const products = await productManager.getProductos()
-    res.render('realtimeProducts', {products})
+router.get('/realtimeProducts', async (req, res) => {try {const products = await baseModel.find().lean().exec()
+    const carro = new cartModel({ products: []})
+    const saveCart = await carro.save()
+    const cart = saveCart._id
+    res.render('realTimeProducts' , {products, cart})
+}catch(err){
+    console.error("Error al crear el carrito:",);
+    res.status(500).json({ status: "error", error: "Error interno del servidor" });
+}
 })
 
 module.exports = router; 
